8e12c398530b0bc59d1783f8f497036b17fc02de
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-03-10 11:11:44 +0300

    qwe

--- src/main/java/ru/hse/restaurant/project/service/AuthService.java
+++ src/main/java/ru/hse/restaurant/project/service/AuthService.java
@@ -4,8 +4,8 @@ package ru.hse.restaurant.project.service;
 import ru.hse.restaurant.project.repository.AuthRepository;
 
 public class AuthService {
-//    public AuthRepository authRepository;
-//
+    public AuthRepository authRepository;
+
 //    public Boolean register(String login, String password) {
 //
 //    }
